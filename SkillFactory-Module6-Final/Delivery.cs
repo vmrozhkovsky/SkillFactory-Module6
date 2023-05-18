4d181447672f73bb70ef944d6627928ac653d7fc
vmrozhkovsky <128725885+vmrozhkovsky@users.noreply.github.com>
2023-05-18 18:57:54 +0300

    Добавлен класс Delivery.

--- SkillFactory-Module6-Final/Delivery.cs
+++ SkillFactory-Module6-Final/Delivery.cs
@@ -9,12 +9,36 @@ namespace SkillFactory_Module6_Final
     //Класс доставки создан для демонстрации агрегации. 
     public class Delivery
     {
-        public Delivery(string name, string address) { }
-        
         private Courier<string> OrderCourier;
-        public Delivery(Courier<string> courier)
+        private string DeliveryName;
+        private string DeliveryAddress;
+        private string DeliveryDescription;
+        public enum DeliveryType
+        {
+            HomeDelivery,
+            PostDelivery,
+            Pickup
+        }
+
+        public Delivery(string name, string address, DeliveryType deliverytype, Courier<string> courier)
         {
             this.OrderCourier = courier;
+            this.DeliveryName = name;
+            this.DeliveryAddress = address;
+            if (!(deliverytype == DeliveryType.Pickup) ) 
+            {
+                Console.WriteLine("Тип доставки {0}, имя курьера - {1}", deliverytype, OrderCourier.UserName);
+            }
+            else
+            {
+                Console.WriteLine("Выбран самолвывоз.");
+            }
+        }
+        
+        public Delivery(string name, Courier<string> courier)
+        {
+            this.OrderCourier = courier;
+            this.DeliveryName = name;
         }
 
     }
